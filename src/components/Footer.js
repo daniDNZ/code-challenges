69773d40961ba5420e07cc4dfa1fd041b34833ef
danisanz <danielsanzelguer@gmail.com>
2022-05-29 21:22:22 +0200

    Fixed problems react-router-dom with gh-pages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <>
       <footer className="footer">
-        <span className="footer__span">Creado con ❤ por &nbsp;<a href="http://www.danisanzdev.com" className="a">Dani Sanz</a></span>
+        <span className="footer__span">Creado con ❤ por &nbsp;<a href="http://www.danisanzdev.com" className="footer__a a">Dani Sanz</a></span>
       </footer>
     </>
   );
